b84d3d4466af207ce71829b8dfdeb8ea40dedef2
Jeff Williams <jwilliams@leafhome.com>
2022-10-18 16:48:12 -0400

    Creating cart service; allow users to add and review their cart

---
+++ Models/Product.cs
@@ -0,0 +1,10 @@
+﻿namespace BlazorApp.Models
+{
+    public class Product
+    {
+        public Guid Id { get; set; }
+        public string Name { get; set; } = "";
+        public string Description { get; set; } = "";
+        public double Price { get; set; }
+    }
+}
---
+++ States/CartState.cs
@@ -0,0 +1,27 @@
+﻿using System.Net.Http.Json;
+using BlazorApp.Models;
+
+namespace BlazorApp.States;
+
+public class CartState
+{
+    private readonly HttpClient _httpClient;
+
+    public CartState(HttpClient httpClient)
+    {
+        _httpClient = httpClient;
+    }
+
+    public List<Product> SelectedItems { get; set; } = new();
+
+    public async Task AddProductToCartAsync(Guid productId)
+    {
+        if (SelectedItems.Any(p => p.Id == productId) is false)
+        {
+            var products = await _httpClient.GetFromJsonAsync<List<Product>>("sample-data/products.json") ??
+                           new List<Product>();
+            var product = products.First(p => p.Id == productId);
+            SelectedItems.Add(product);
+        }
+    }
+}
